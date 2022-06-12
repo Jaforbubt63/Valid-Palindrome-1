4d44bb353d7d063342a1e62578c3ba94de7367f0
Abu Jafor <103594222+Jaforbubt63@users.noreply.github.com>
2022-06-12 22:05:42 +0600

    Add files via upload

---
+++ Valid Palindrome.py
@@ -0,0 +1,14 @@
+import re
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        s = re.sub('[\w_]', '', s)
+        
+        i, j = 0, len(s) - 1
+        while i < j:
+            if s[i] != s[j]:
+                return False
+            i += 1
+            j -= 1
+            return True
+        
